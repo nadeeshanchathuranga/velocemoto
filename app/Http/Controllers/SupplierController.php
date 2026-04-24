2ea5241c82700297b01028bebbbabf2d5e4a5017
THILAKARATHNA A H M T L <it23160798@my.sliit.lk>
2026-04-24 11:48:33 +0530

    supplier fix

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -67,8 +67,8 @@ class SupplierController extends Controller
         $validated = $request->validate([
             'name' => 'required|string|max:191|regex:/^[a-zA-Z0-9\s]+$/',
            'contact' => 'required|string|regex:/^\d{10}$/',
-            'email' => 'required|email|regex:/^[\w\.-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,6}$/|max:255|unique:suppliers,email',
-            'address' => 'required|string|max:500',
+            'email' => 'nullable|email|regex:/^[\w\.-]+@[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,6}$/|max:255|unique:suppliers,email',
+            'address' => 'nullable|string|max:500',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:1024',
         ]);
 
